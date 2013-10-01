429cba685a6d17f5eb28865c4790d1017822fb12
Jan Stransky <jan.stransky@arnal.cz>
2013-10-01 10:03:23 +0200

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@
 # Bootstrap
 # Basic things as install package manager and Ansible.
 
+MYSETUP_INSTALL_URL="https://raw.github.com/burningtree/mysetup/master/tools/install.sh"
+MYSETUP_TAP="https://github.com/burningtree/ansible-playbooks"
+
 os=`uname -s`
 
 ###########################################
@@ -39,7 +42,7 @@ if [ $os = Darwin ]; then
   # basic brew packages                     #
   ###########################################
 
-  packages="ack ansible git"
+  packages="ack ansible"
   echo "Checking for basic brew packages [ $packages ] .."
 
   for pkg in $packages; do
@@ -49,7 +52,7 @@ if [ $os = Darwin ]; then
       echo "Installing brew package: $pkg .."
 
       `brew install $pkg`
-      if [ !$? ]; then
+      if [ $? != 0 ]; then
         echo "Error: failed installing package: $pkg"
       fi
     fi
@@ -57,10 +60,40 @@ if [ $os = Darwin ]; then
     echo "Package: $(brew info $pkg | ack "$pkg: (\w+ \d+)")"
   done
 
-  echo "Bootstrap done!"
-
 else
   echo "Error: Unknown OS: $os"
   exit 1
 fi
+
+###########################################
+# OS independent                          #
+###########################################
+
+###########################################
+# mysetup installation                    #
+###########################################
+
+# check if installed
+mysetup_dir="$HOME/.mysetup"
+mysetup_bin="$mysetup_dir/bin/mysetup"
+
+if [ ! -f $mysetup_bin ]; then
+  echo "Installing mysetup .."
+  curl -s $MYSETUP_INSTALL_URL | sh >/dev/null
+
+  if [ $? != 0 ]; then
+    echo "error: installation of 'mysetup' failed"
+    exit 10
+  fi
+fi
+
+mysetup_version=`$mysetup_bin version`
+echo "mysetup: $mysetup_version"
+
+if [ ! -d "$mysetup_dir/repo" ]; then
+  echo "Tapping config in mysetup .."
+  $HOME/.mysetup/bin/mysetup tap $MYSETUP_TAP >/dev/null
+fi
+
+echo "Bootstrap done!"
 
